abd2e15bc911d97251acfbc2d413a7b53fbaa642
DiitsGp <aditya@gopalans.net>
2015-05-29 11:40:43 -0400

    Added plotting. Need to add a visualizer at some point too. Also started rounding some of the hard data to make it a bit less noisy. Probably use a stronger filter instead in the future.

--- contactBasedStateEstimator.m
+++ contactBasedStateEstimator.m
@@ -1,4 +1,4 @@
-function [r,xtraj,utraj,ltraj,z,F,info,prog] = ContactBasedStateEstimator(xtraj,utraj,ltraj,ljltraj)
+function [r,xtraj,utraj,ltraj,z,F,info,prog] = contactBasedStateEstimator(xtraj,utraj,ltraj,ljltraj)
 %ContactBasedStateEstimator
 %   Estimate states given linear acceleration data and angular velocity
 %   data otbained from an onboard IMU in a planar falling brick
@@ -13,7 +13,7 @@ OptiData = csvread('Mar312015Sample01_2d.csv'); %time, x, y, theta
 [b, a] = butter(1, 0.2);
 times = OptiData(:, 1);
 x_filt = filtfilt(b, a, OptiData(:, 2));
-y_filt = filtfilt(b, a, OptiData(:, 3)) - OptiData(end, 3) + 0.0353;
+y_filt = filtfilt(b, a, OptiData(:, 3)) - OptiData(end, 3);
 theta_filt = filtfilt(b, a, OptiData(:, 4))*pi/180;
 dt = diff(times);
 xdot = diff(x_filt) ./ dt(1:end);
@@ -46,30 +46,26 @@ for i=25:77
 end
 G = -mean(G);
 Gdir = mean(Gdir) + pi;
-
-%LOLOLOL redo this with R = Rotx(ang)
-%State = R*[x_filt, y_filt, 0];
-%x = State(1) etc
-Rot = [cos(Gdir), -sin(Gdir); sin(Gdir), cos(Gdir)];
+Rot = rotx(Gdir);
+Rot = [cos(Gdir), -sin(Gdir), 0; sin(Gdir), cos(Gdir), 0; 0, 0, 0];
 for i = 1:length(x_filt)
-    State = Rot*[x_filt(i); y_filt(i)];
+    State = Rot*[x_filt(i); y_filt(i); 0];
     x_filt(i) = State(1);
     y_filt(i) = State(2);
-    Vel = Rot*[xdot(i); ydot(i)];
+    Vel = Rot*[xdot(i); ydot(i); 0];
     xdot(i) = Vel(1);
     ydot(i) = Vel(2);
-    Acc = Rot*[xddot(i); yddot(i)];
+    Acc = Rot*[xddot(i); yddot(i); 0];
     xddot(i) = Acc(1);
     yddot(i) = Acc(2);
     theta_filt(i) = theta_filt(i) + Gdir;
 end
 
-%%setup
+%% setup
 options.terrain = RigidBodyFlatTerrain();
 options.floating = true;
 options.dt = 0.01;
 w = warning('off','Drake:RigidBodyManipulator:UnsupportedContactPoints');
-%x0 = [0 1 0 0 0 0]';
 
 N = 15;
 tf = times(end) - times(1);
@@ -96,6 +92,7 @@ r = TimeSteppingRigidBodyManipulator(p, options.dt);
 options = struct();
 options.integration_method = ContactImplicitTrajectoryOptimization.MIXED;
 
+%% do trajectory optimization
 %scale_sequence = [1;.001;0];
 prog = ContactImplicitTrajectoryOptimization(r.getManipulator,2,tf,options);
 prog = prog.setSolverOptions('snopt','MajorIterationsLimit',200);
@@ -126,17 +123,26 @@ for i=2:N %length(scale_sequence)
         uIMU = [0; 0; 0];
         for k = inds(j-1)+1:inds(j)
            dt = times(k) - times(k-1);
-           uIMU(1) = uIMU(1) + dt*xddot(k);
-           uIMU(2) = uIMU(2) + dt*yddot(k);
+%            if j > 8
+%                uIMU(2) = uIMU(2) + 0;
+%                uIMU(1) = uIMU(1) + 0;
+%            else
+               uIMU(1) = uIMU(1) + dt*round(xddot(k), 2);
+               uIMU(2) = uIMU(2) + dt*round(yddot(k), 2);
+%            end
         end
-        uIMU(3) = thetadot(inds(j));
+%         if j > 8
+%             uIMU(3) = 0;
+%         else
+            uIMU(3) = round(thetadot(inds(j)), 2);
+%         end
         IMU_fun = @(x, oldx) IMUcost(x, oldx, uIMU);
         IMUerr_cost = FunctionHandleObjective(2,IMU_fun);
         prog = addCost(prog,IMUerr_cost,{prog.x_inds(4:6, j); prog.x_inds(4:6, j-1)});
         
 %        uGT = [x_gt(j, 1); x_gt(j, 2); x_gt(j, 3)];
 %        GT_fun = @(x) GTcost(x, uGT);
-%        GTerr_cost = FunctionHandleObjective(1, GT_fun);s
+%        GTerr_cost = FunctionHandleObjective(1, GT_fun);
 %        prog = prog.addCost(GTerr_cost, {prog.x_inds(1:3, j)'});
     end
     
@@ -151,18 +157,56 @@ for i=2:N %length(scale_sequence)
     toc
 end
 
+%% plot the final errors
+for i = 1:N
+    t = times(inds(i));
+    xtrajfinal = xtraj.eval(t);
+    
+    x_calc(i) = xtrajfinal(1);
+    y_calc(i) = xtrajfinal(2);
+    theta_calc(i) = xtrajfinal(3);
+    
+    x_err(i) = (x_gt(i, 1) - x_calc(i))/x_gt(i, 1);
+    y_err(i) = (x_gt(i, 2) - y_calc(i))/x_gt(i, 2);
+    theta_err(i) = (x_gt(i, 3) - theta_calc(i))/x_gt(i, 3);
+end
+
+figure
+stem(times(inds), x_gt(:, 1));
+title('gt-x vs times');
+
+figure
+stem(times(inds), x_gt(:, 2));
+title('gt-y vs times');
+
+figure
+stem(times(inds), x_gt(:, 3));
+title('gt-theta vs times');
+
+figure
+stem(times(inds), x_calc);
+title('x-calc at knot points');
+
+figure
+stem(times(inds), y_calc);
+title('y-calc at knot points');
+
+figure
+stem(times(inds), theta_calc);
+title('theta-calc at knot points');
+
+drawnow;
+keyboard
+
 %% cost fun!
-
-
-
     function [f, df] = IMUcost(x, oldx, u)
         diffvel = [x(1)-oldx(1); x(2)-oldx(2); x(3)];
         Q = [2, 0, 0; 0, 2, 0; 0, 0, 1]; % Q is a scaling matrix
         f = (diffvel-u)'*Q*(diffvel-u);
-        ddiffvel = [1, 0, 0, -1, 0, 0;...
+        graddiffvel = [1, 0, 0, -1, 0, 0;...
             0, 1, 0, 0, -1, 0;...
             0, 0, 1, 0, 0, 0];
-        df = 2*(diffvel-u)'*Q*ddiffvel;
+        df = 2*(diffvel-u)'*Q*graddiffvel;
     end
 
 %     function [f, df] = GTcost(x, u)
