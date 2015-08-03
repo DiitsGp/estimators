2151cbf86b24a76af0e490b092eb8eef0ae4357d
DiitsGp <aditya@gopalans.net>
2015-08-03 12:00:41 -0400

    Refactored elastic lcpsolve to work with a different Anitescu paper that resolves elastic contacts with a fixed timestep

--- elasticLCP.m
+++ elasticLCP.m
@@ -1,121 +1,110 @@
 function [states, times] = elasticLCP(r, x0, tf)
 % Solves an elastic LCP as described in Anitescu and Potra 1997
 
+% Re-written to use the forumation in Anitescu 2003, "A Fixed Time-Step
+% Approach for Multibody Dynamics with Contact and Friction"
+
 states = x0;
 times = 0;
-phi_tol = 1e-4;
+phi_tol = 1e-6;
 h = 0.001;
 t = 0;
 q = x0(1:3);
 v = x0(4:6);
-eps = 0.1;%0.005; % coefficient of restitution
+vlast = 0;
+eps = 0; % coefficient of restitution
 while(t < tf)
-%     if (t > 0.5092)
-%         keyboard;
-%     end
+    %     if (t > 0.5092)
+    %         keyboard;
+    %     end
     [M, C] = r.manipulatorDynamics(q, v, 0);
+    %
+    %     qnew = q + h*v;
+    %     kinsol = r.doKinematics(qnew);
+    %     [phi, ~, ~, ~, ~, ~, ~, mu, n, D] = r.contactConstraints(kinsol, false);
+    %
+    %     xd = r.dynamics(0, [q;v], 0);
+    %     vnew = v + h*xd(4:6);
+    %     %z = anitescuLCP(M, v, -h*C, mu, n, D, ); % to find vnew
+    %     %vnew = z(1:3)
+    %
+    %     if ((~ any(phi < phi_tol)) | (v == 0))
+    %         q = qnew;
+    %         vlast = v;
+    %         v = vnew;
+    % %         disp('no');
+    %         t = t+h;
+    %         states = [states, [q; v]];
+    %         times = [times, t];
+    %     else
+    %         contact_inds = find(phi < phi_tol);
     
-    qnew = q + h*v;
-    kinsol = r.doKinematics(qnew);
+    kinsol = r.doKinematics(q, v);
     [phi, ~, ~, ~, ~, ~, ~, mu, n, D] = r.contactConstraints(kinsol, false);
-    D = vertcat(D{:});
-
-    xd = r.dynamics(0, [q;v], 0);
-    vnew = v + h*xd(4:6);
-    %z = anitescuLCP(M, v, -h*C, mu, n, D, ); % to find vnew
-    %vnew = z(1:3)
     
-    if ((~ any(phi < phi_tol)) | (v == 0))
-        q = qnew;
-        v = vnew;
-%         disp('no');
-        t = t+h;
-        states = [states, [q; v]];
-        times = [times, t];
+    contact_inds = find(phi < phi_tol);
+    D = vertcat(D{:});
+    % Anitescu says it should be Delta = 1/h*phi(contact_inds); but for
+    % some reason this induces an energy gain
+    Delta = zeros(numel(contact_inds), 1); 
+    vn = n(contact_inds, :)*vlast;
+    if (isempty(vn))
+        Lambda = [];
     else
-%         if (t >  0.440380730094043)
-%             keyboard;
-%         end
-        % linesearch to find collision time with a gap function tolerance
-        % of phi_tol
-        hhl = 0;
-        hhu = h;
-        hhm = h/2;
-        err = min(phi);
-
-        while (err < 0 || err > phi_tol)
-            hhm = (hhl+hhu)/2;
-            % added the following block to aid convergence...need to find
-            % the sweet spot for the step limit of hhm
-%             if (hhm < 1e-5)
-%                 hhm = 1e-5;
-%                 break;
-%             end
-            % back to normal operation here
-            kinsol = r.doKinematics(q + hhm*v);
-            phi = r.contactConstraints(kinsol, true);
-            if (min(phi) >= 0)
-                hhl = hhm;
-            else
-                hhu = hhm;
-            end
-            
-            err = min(phi);
-        end
-        
-        hh = hhm; %% hh is the (smaller) timestep to add to t
-        qnew = q + hh*v;
-        xd = r.dynamics(0, [q; v]); % +++TK: YOU WERE CALLING r.dynamics(0, q, v) here, while dynamics expects t, x, u as arguments
-        vminus = v + hh*xd(4:6); % euler step to approximately guess vminus with new timestep
-        
-%         keyboard;
-        
-        contact_inds = find(phi < phi_tol);
-        z = anitescuLCP(M, vminus, 0, mu(contact_inds), n(contact_inds, :),...
-            D([contact_inds, contact_inds+4], :), numel(contact_inds));% to find vc
-        vc = z(1:3);
-        cn = z(4:3+numel(contact_inds)); % contact normal force
-        Fr = eps*n(contact_inds, :)'*cn; % restitution impulse
-        
-        %%% if it doesn't work, update mu, n, D with contactConstraints at
-        %%% qcollision
-%         kinsol = r.doKinematics(qnew);
-%         [~, ~, ~, ~, ~, ~, ~, mu, n, D] = r.contactConstraints(kinsol, false);
-%         D = vertcat(D{:});
-        %%% ok
-        z = anitescuLCP(M, vc, Fr, mu(contact_inds), n(contact_inds, :),...
-            D([contact_inds, contact_inds+4], :), numel(contact_inds));
-        vplus = z(1:3);
-        v = vplus;
-        q = qnew;
-        t = t+hh
-        states = [states, [q; v]];
-        times = [times, t];
-%         keyboard;
+        Lambda = eps*vn.*ones(numel(contact_inds), 1);
     end
+    
+    impulse = -h*C; % h*(B*u-C) --> presently no input
+    z = anitescuLCP(M, v, impulse, Delta, Lambda, mu(contact_inds),...
+        n(contact_inds, :), D([contact_inds, contact_inds+4], :), numel(contact_inds));
+
+    vlast = v;
+    v = z(1:3);
+    q = q + h*v;
+    t = t+h
+    
+    states = [states, [q; v]];
+    times = [times, t];
+end
+% end
+
+xtraj_elastic = PPTrajectory(foh(times, states));
+vv = r.constructVisualizer();
+xtraj_elastic = xtraj_elastic.setOutputFrame(r.getStateFrame);
+vv.playback(xtraj_elastic, struct('slider', true));
+
+keyboard;
 end
 
-% keyboard;
-end
 
-
-function [z] = anitescuLCP(M, v, impulse, mu, n, D, nC)
+function [z] = anitescuLCP(M, v, impulse, Delta, Lambda, mu, n, D, nC)
+if (nC > 0) % to avoid path complaining about empty matrices in the falling case
+    display('Collision!');
     mcpmat = [M, -n', -D', zeros(3, nC);...
         n, zeros(nC, 4*nC);
         D, zeros(2*nC, 3*nC), [eye(nC); eye(nC)];...
         zeros(nC, 3), diag(mu), -eye(nC), -eye(nC), zeros(nC, nC)];
-    mcpvec = [-M*v-impulse; zeros(4, 1)];
-
+    mcpvec = [-M*v-impulse; Delta+Lambda; zeros(3*nC, 1)];
+    
     M = mcpmat(1:3, 1:3);
     H = -mcpmat(1:3, 4:end);
     N = mcpmat(4:end, 4:end);
     
     b = mcpvec(1:3);
-
+    w = mcpvec(4:end);
+    
     lcpmat = H'*(M\H) + N;
-    lcpvec = -H'*(M\b);
+    lcpvec = w-H'*(M\b);
+    
     z = pathlcp(lcpmat, lcpvec, zeros(numel(lcpvec),1), inf(numel(lcpvec),1));
     
     x = M\(H*z - b);
     z = [x; z];
+else
+    lcpmat = M;
+    lcpvec = -M*v-impulse;
+    
+    z = pathlcp(lcpmat, lcpvec, -inf(numel(lcpvec), 1), inf(numel(lcpvec), 1));
+end
+
 end
--- test_elastic.m
+++ test_elastic.m
@@ -4,7 +4,7 @@ options.dt = 0.001;
 options.use_bullet = false;
 options.enable_fastqp = false;
 options.ignore_self_collisions = true;
-options.use_new_kinsol = true;
+% options.use_new_kinsol = true;
 
 w = warning('off','Drake:RigidBodyManipulator:UnsupportedContactPoints');
 urdf = fullfile('Planar_CBSE_Window.URDF');
@@ -15,14 +15,110 @@ p = PlanarRigidBodyManipulator(urdf, options);
 p = p.setGravity([0; 0; G]);
 r = TimeSteppingRigidBodyManipulator(p, options.dt, options);
 
-tf = 10;
-x0 = [0; 1; pi/4; 0.5; 0; pi];
+tf = 2;
+x0 = [0; 1; pi/4; 0; 0; 0];
 
 p = p.compile();
 
-[states, times] = elasticLCP(p, x0, 2);
+[states, times] = elasticLCP(p, x0, tf);
 
 xtraj_elastic = PPTrajectory(foh(times, states));
 v = r.constructVisualizer();
 xtraj_elastic = xtraj_elastic.setOutputFrame(r.getStateFrame);
-v.playback(xtraj_elastic, struct('slider', true))
+v.playback(xtraj_elastic, struct('slider', true));
+
+% traj = xtraj_elastic.eval(xtraj_elastic.getBreaks());
+% x = traj(1, :);
+% z = traj(2, :);
+% theta = traj(3, :);
+% xdot = traj(4, :);
+% zdot = traj(5, :);
+% thetadot = traj(6, :);
+% 
+% times = linspace(0, tf, length(x));
+% dt = diff(times);
+% sensor_inds = round(linspace(1, numel(xtraj_elastic.getBreaks()), 120*tf));
+% sensor_inds = sensor_inds(1:50);
+% tempxdot = xdot(sensor_inds);
+% tempzdot = zdot(sensor_inds);
+% temptimes = times(sensor_inds);
+% xddot = diff(tempxdot)./diff(temptimes);
+% zddot = diff(tempzdot)./diff(temptimes);
+% theta_dot = thetadot(sensor_inds);
+% sensor_inds = sensor_inds(1:numel(sensor_inds)-1);
+% 
+% for i = 1:numel(xddot)
+%    j = sensor_inds(i);
+%    ang = theta(j);
+%    R = [cos(ang), sin(ang); -sin(ang), cos(ang)];
+%    val = R*[xddot(i); zddot(i)];
+%    xddot(i) = val(1);
+%    zddot(i) = val(2);
+% end
+% 
+% N = numel(sensor_inds);
+% 
+% times = times(1:numel(times)-1) - times(1);
+% times = times';
+% 
+% noisy_thetadot = theta_dot(2:end);
+% 
+% inds = round(linspace(1, size(times, 1), N));
+% 
+% % figure
+% % plot(times(inds), theta(inds), '*');
+% traj_sim = xtraj_elastic;
+% ts_sim = traj_sim.getBreaks();
+% traj_sim = PPTrajectory(foh(ts_sim,traj_sim.eval(ts_sim)));
+% ts = times(sensor_inds);
+% non_meas = zeros(numel(xddot), 1);
+% data = [ts, xddot', non_meas, zddot', non_meas, noisy_thetadot', non_meas];
+% [r, xtraj, utraj, ltraj, zz, F, info, prog] = contactBasedStateEstimator(r, N, x0, data, traj_sim);
+% 
+% traj_ts = xtraj.getBreaks();
+% traj_eval = xtraj.eval(traj_ts);
+% x_calc = traj_eval(1, :);
+% z_calc = traj_eval(2, :);
+% theta_calc = traj_eval(3, :);
+% xdot_calc = traj_eval(4, :);
+% zdot_calc = traj_eval(5, :);
+% thetadot_calc = traj_eval(6, :);
+% 
+% % onlz plot the results of integration here
+% plot_inds = round(linspace(1, numel(xtraj_ts.tt), 120*tf));
+% plot_inds = plot_inds(2:50);
+% plotx = x(plot_inds);
+% plotz = z(plot_inds);
+% plottheta = theta(plot_inds);
+% plotxdot = xdot(plot_inds);
+% plotzdot = zdot(plot_inds);
+% plotthetadot = thetadot(plot_inds);
+% 
+% figure
+% subplot(3, 2, 1)
+% plot(times(sensor_inds), plotx, '+', times(sensor_inds), x_calc, '*');
+% title('sim-x (+) and x-calc (*) vs times');
+% 
+% subplot(3, 2, 3)
+% plot(times(sensor_inds), plotz, '+', times(sensor_inds), z_calc, '*');
+% title('sim-z (+) and z-calc (*) vs times');
+% 
+% subplot(3, 2, 5)
+% plot(times(sensor_inds), plottheta, '+', times(sensor_inds), theta_calc, '*');
+% title('sim-theta (+) and theta-calc (*) vs times');
+% 
+% subplot(3, 2, 2)
+% plot(times(sensor_inds), plotxdot, '+', times(sensor_inds), xdot_calc, '*');
+% title('sim-xdot (+) and xdot-calc (*) vs times');
+% 
+% subplot(3, 2, 4)
+% plot(times(sensor_inds), plotzdot, '+', times(sensor_inds), zdot_calc, '*');
+% title('sim-zdot (+) and zdot-calc (*) vs times');
+% 
+% subplot(3, 2, 6)
+% plot(times(sensor_inds), plotthetadot, '+', times(sensor_inds), thetadot_calc, '*');
+% title('sim-thetadot (+) and thetadot-calc (*) vs times');
+% 
+% drawnow;
+% 
+% display(['Cost: ', num2str(prog.objective(zz))]);
